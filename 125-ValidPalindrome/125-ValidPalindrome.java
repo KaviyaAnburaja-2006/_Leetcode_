c7cc98b30058bc741dd7170e754fc1eea9f1f6df
Kaviya A <kaviyaanburaja2006@gmail.com>
2026-07-14 14:15:06 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 2:14:01 PM
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        s=s.replaceAll("[^a-z0-9]","");
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            rev+=s.charAt(i);
+        }
+        return s.equals(rev);
+    }
+    
+}
